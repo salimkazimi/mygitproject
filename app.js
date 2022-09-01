7414fcd3dd526a8c13011156b3ab5d7b9b7a6817
salim <salim.salih1991@gmail.com>
2022-09-01 17:31:03 +0430

    login page edited

--- app.js
+++ app.js
@@ -23,7 +23,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app .use(expresslayout)
 app.use('/', indexRouter);
-app.use('/', loginRouter)
+app.use('/users', usersRouter);
+app.use('/login', loginRouter)
 
 
 // catch 404 and forward to error handler
