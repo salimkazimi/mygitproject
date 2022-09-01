8594b8f556c1cf1a3579b986355e67f026ee2bbd
salim <salim.salih1991@gmail.com>
2022-09-01 17:33:45 +0430

    login page edited 2

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var expresslayout = require('express-ejs-layouts')
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var loginRouter = require('./routes/login')
-var expresslayout = require('express-ejs-layouts')
+var loginRouter = require('./routes/login');
+
 var app = express();
 
 // view engine setup
@@ -15,13 +17,15 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.set('layout' , './layout')
 
+
+app .use(expresslayout)
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app .use(expresslayout)
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/login', loginRouter)
